333009180846aa01f36feb0ec0addebfb6ea9c2e
Lefi0305 <huongbdno1@gmail.com>
2025-12-06 14:27:06 +0700

    Update contacts.py

--- contacts.py
+++ contacts.py
@@ -1,11 +1,8 @@
-phonebook = []
-
-def add_contact():
-    name = input("Nhập tên: ")
-    phone = input("Nhập số điện thoại: ")
+def view_contacts():
+    if not phonebook:
+        print("Danh bạ rỗng.")
+        return
     
-    contact = {'name': name, 'phone': phone}
-    phonebook.append(contact)
-
-    print("Đã thêm liên hệ.")
-
+    print("\n--- DANH SÁCH LIÊN HỆ ---")
+    for i, contact in enumerate(phonebook, start=1):
+        print(f"{i}. {contact['name']} - {contact['phone']}")
